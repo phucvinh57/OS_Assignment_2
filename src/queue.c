6a44a1d963c8d05e1320047412b15b2b6718d1e3
thuongle2210 <thuong.le2210@hcmut.edu.vn>
2021-05-28 10:32:43 +0700

    scheduler

--- src/queue.c
+++ src/queue.c
@@ -8,12 +8,29 @@ int empty(struct queue_t * q) {
 
 void enqueue(struct queue_t * q, struct pcb_t * proc) {
 	/* TODO: put a new process to queue [q] */	
+	if (q->size == MAX_QUEUE_SIZE) 
+		return;
+	q->proc[q->size] = proc;
+	q->size++;
 }
 
 struct pcb_t * dequeue(struct queue_t * q) {
 	/* TODO: return a pcb whose prioprity is the highest
 	 * in the queue [q] and remember to remove it from q
 	 * */
-	return NULL;
+	if (q->size == 0) 
+		return NULL;
+	int vt = 0;
+	for (int i = 1; i < q->size; i++) {
+		if (q->proc[i]->priority > q->proc[vt]->priority) {
+			vt = i;
+		}
+	}
+	struct pcb_t * result = q->proc[vt];
+	for (int i = vt+1; i < q->size; i++) {
+		q->proc[i-1] = q->proc[i];
+	}
+	q->size--;
+	return result;
 }
 
